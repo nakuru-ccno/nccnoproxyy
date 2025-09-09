3fa953cf6319262067b8a6952796181a7ef5c230
nakuru-ccno <nakurucountychiefnursingoffice@gmail.com>
2025-09-09 09:51:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" })); // JSON body
 
 // Replace with your Google Apps Script URL
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzI7j4TKYRe4U6FKFgbCVf3tVPwPdm5VHdRqKzgOtMffmQ0U7eosiHs8b2j2xnVcIBq-A/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzPpiz7OsP_nuuVXEPxvfGBgOdPMuhDQ-mB9M1nDW3eb4wmV6KhG3g88uiNljNoWaLN/exec";
 
 // Health check
 app.get("/health", (req, res) => {
